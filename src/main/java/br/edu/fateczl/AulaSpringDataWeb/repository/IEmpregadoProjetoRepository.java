03f6e88c879c4f0cd98856ff332e91f573f4fa50
Leandro Colevati <lecolevati@yahoo.com.br>
2023-11-08 11:46:13 -0300

    Update IEmpregadoProjetoRepository.java

--- src/main/java/br/edu/fateczl/AulaSpringDataWeb/repository/IEmpregadoProjetoRepository.java
+++ src/main/java/br/edu/fateczl/AulaSpringDataWeb/repository/IEmpregadoProjetoRepository.java
@@ -1,9 +1,10 @@
-package br.edu.fateczl.AulaSpringDataWeb.repository;
+package br.edu.fateczl.ExemploSpringData.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.edu.fateczl.AulaSpringDataWeb.model.EmpregadoProjeto;
+import br.edu.fateczl.ExemploSpringData.model.EmpregadoProjeto;
+import br.edu.fateczl.ExemploSpringData.model.EmpregadoProjetoId;
 
-public interface IEmpregadoProjetoRepository extends JpaRepository<EmpregadoProjeto, Integer> {
+public interface IEmpregadoProjetoRepository extends JpaRepository<EmpregadoProjeto, EmpregadoProjetoId>{
 
 }
